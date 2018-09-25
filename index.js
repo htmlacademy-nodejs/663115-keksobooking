a957c7a53bd517244dc65f5d38c15713dc04dc26
Alexander <sitesmake@gmail.com>
2018-09-25 18:03:13 +0300

    Move messages into object

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-if (process.argv.length == 2) {
-  console.log(`
+const messages = {
+  info: `
     Привет пользователь!
     Эта программа будет запускать сервер «Keksobooking».
-    Автор: Александр Грищенко.
-  `);
-  process.exit(0);
-} else if (process.argv.includes('--version')) {
-  console.log(`v0.0.1`);
-  process.exit(0);
-} else if (process.argv.includes('--help')) {
-  console.log(`
+    Автор: Александр Грищенко.`,
+
+  version: `v0.0.1`,
+
+  help: `
     Доступные команды:
     --help    — печатает этот текст;
-    --version — печатает версию приложения;
-  `);
+    --version — печатает версию приложения;`
+};
+
+if (process.argv.length == 2) {
+  console.log(messages['info']);
+
   process.exit(0);
+
+} else if (process.argv.includes('--version')) {
+  console.log(messages['version']);
+
+  process.exit(0);
+
+} else if (process.argv.includes('--help')) {
+  console.log(messages['help']);
+
+  process.exit(0);
+
 } else {
-  console.log(`
+  console.error(`
     Неизвестная команда ${process.argv[2]}.
     Чтобы прочитать правила использования приложения, наберите "--help"
   `);
+
   process.exit(1);
-}
+};
