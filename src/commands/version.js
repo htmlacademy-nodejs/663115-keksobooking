c0c2452a1689667fba4f75615a6237dfbffbf163
Alexander <sitesmake@gmail.com>
2018-09-30 18:36:27 +0300

    Add colors to version output

--- src/commands/version.js
+++ src/commands/version.js
@@ -1,11 +1,17 @@
 'use strict';
 
+require(`colors`);
 const Message = require(`../message`);
+
+const withColors = (version) => {
+  const versionParts = version.slice(1).split(`.`);
+  return `v${versionParts[0].red}.${versionParts[1].green}.${versionParts[2].blue}`;
+};
 
 module.exports = {
   name: `--version`,
   description: `Показывает версию`,
   execute() {
-    console.log(Message.VERSION);
+    console.log(withColors(Message.VERSION));
   }
 };
