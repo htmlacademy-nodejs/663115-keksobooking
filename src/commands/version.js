a046f03f34e008000da6cdb9bcd4e764548fb334
Alexander <sitesmake@gmail.com>
2018-09-30 23:58:44 +0300

    Change format version function name

--- src/commands/version.js
+++ src/commands/version.js
@@ -4,7 +4,7 @@ require(`colors`);
 
 const Message = require(`../message`);
 
-const withColors = (version) => {
+const formatVersionStringWithColors = (version) => {
   const versionParts = version.slice(1).split(`.`);
   return `v${versionParts[0].red}.${versionParts[1].green}.${versionParts[2].blue}`;
 };
@@ -13,6 +13,6 @@ module.exports = {
   name: `--version`,
   description: `Показывает версию`,
   execute() {
-    console.log(withColors(Message.VERSION));
+    console.log(formatVersionStringWithColors(Message.VERSION));
   }
 };
