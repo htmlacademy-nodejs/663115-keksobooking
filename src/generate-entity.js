e27227564af6286ed59c865f2ba1a931e2194596
Alexander <sitesmake@gmail.com>
2018-10-05 22:45:44 +0300

    Generate entity with tests

--- src/generate-entity.js
+++ src/generate-entity.js
@@ -1,13 +1,27 @@
 'use strict';
 
-const randomItem = require(`random-item`);
-const randomInt = require(`random-int`);
-
 const X_MIN = 300;
 const X_MAX = 900;
 const Y_MIN = 150;
 const Y_MAX = 500;
+const PRICE_MIN = 1000;
+const PRICE_MAX = 1000000;
+const ROOMS_MIN = 1;
+const ROOMS_MAX = 5;
 
+const randomItem = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+const randomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const randomSortArray = (ary) => {
+  return ary.sort(function () {
+    return 0.5 - Math.random();
+  });
+};
 
 const mockAvatar = () => {
   return `https://robohash.org/${Math.random().toString(36).substring(2, 15)}`;
@@ -21,6 +35,48 @@ const mockAddress = (x, y) => {
   return x + `,` + y;
 };
 
+const mockPrice = (min, max) => {
+  return randomInt(min, max);
+};
+
+const mockType = () => {
+  return randomItem([`palace`, `flat`, `house`, `bungalo`]);
+};
+
+const mockRooms = () => {
+  return randomInt(ROOMS_MIN, ROOMS_MAX);
+};
+
+const mockGuests = () => {
+  return randomInt(ROOMS_MIN, ROOMS_MAX) * 2;
+};
+
+const mockCheckin = () => {
+  return randomItem([`12:00`, `13:00`, `14:00`]);
+};
+
+const mockCheckout = () => {
+  return randomItem([`12:00`, `13:00`, `14:00`]);
+};
+
+const mockFeatures = () => {
+  const featuresData = randomSortArray([`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`]);
+  const featuresSize = randomInt(1, featuresData.length);
+  let features = [];
+  for (let i = 0; i < featuresSize; i++) {
+    features.push(featuresData[i]);
+  }
+  return features;
+};
+
+const mockDescription = () => {
+  return ``;
+};
+
+const mockPhotos = () => {
+  return randomSortArray([`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`]);
+};
+
 const generateEntity = () => {
   const locationX = randomInt(X_MIN, X_MAX);
   const locationY = randomInt(Y_MIN, Y_MAX);
@@ -31,7 +87,20 @@ const generateEntity = () => {
     },
     offer: {
       title: mockTitle(),
-      address: mockAddress(locationX, locationY)
+      address: mockAddress(locationX, locationY),
+      price: mockPrice(PRICE_MIN, PRICE_MAX),
+      type: mockType(),
+      rooms: mockRooms(),
+      guests: mockGuests(),
+      checkin: mockCheckin(),
+      checkout: mockCheckout(),
+      features: mockFeatures(),
+      description: mockDescription(),
+      photos: mockPhotos()
+    },
+    location: {
+      x: locationX,
+      y: locationY
     }
   };
 };
--- test/generate-entity.test.js
+++ test/generate-entity.test.js
@@ -15,7 +15,7 @@ describe(`Entity`, () => {
   });
 
   it(`should generate a url for avatar`, () => {
-    expect(entity.author.avatar).to.have.string(`https:`);
+    expect(entity.author.avatar).to.have.string(`https:`); // TODO: add regex
   });
 
   it(`should generate a valid string for title`, () => {
@@ -25,4 +25,51 @@ describe(`Entity`, () => {
   it(`should generate address in correct format`, () => {
     expect(entity.offer.address).to.have.string(`,`);
   });
+
+  it(`should generate price in valid range`, () => {
+    expect(entity.offer.price).to.be.within(1000, 1000000);
+  });
+
+  it(`should generate correct type`, () => {
+    expect([`flat`, `palace`, `house`, `bungalo`]).to.include(entity.offer.type);
+  });
+
+  it(`should generate rooms in valid range`, () => {
+    expect(entity.offer.rooms).to.be.within(1, 5);
+  });
+
+  it(`should generate number of guests`, () => {
+    expect(entity.offer.guests).to.be.a(`number`);
+  });
+
+  it(`should generate checkin in correct format`, () => {
+    expect([`12:00`, `13:00`, `14:00`]).to.include(entity.offer.checkin);
+  });
+
+  it(`should generate checkout in correct format`, () => {
+    expect([`12:00`, `13:00`, `14:00`]).to.include(entity.offer.checkout);
+  });
+
+  it(`should generate features as unique elements`);
+
+  it(`should generate description as empty string`, () => {
+    expect(entity.offer.description).to.equal(``);
+  });
+
+  it(`should generate photos as array of strings`, () => {
+    expect(entity.offer.photos).to.be.an(`array`).that.includes(`http://o0.github.io/assets/images/tokyo/hotel1.jpg`);
+    expect(entity.offer.photos).to.be.an(`array`).that.includes(`http://o0.github.io/assets/images/tokyo/hotel2.jpg`);
+    expect(entity.offer.photos).to.be.an(`array`).that.includes(`http://o0.github.io/assets/images/tokyo/hotel3.jpg`);
+    expect(entity.offer.photos.length).to.be.eq(3);
+  });
+
+  it(`should generate location x in valid range`, () => {
+    expect(entity.location.x).to.be.within(300, 900);
+  });
+
+  it(`should generate location y in valid range`, () => {
+    expect(entity.location.y).to.be.within(150, 500);
+  });
+
+  it(`should generate date in valid format`);
 });
