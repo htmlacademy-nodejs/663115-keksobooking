46d1ed61fce48f202ef69dbb09f2f774567da483
Alexander <sitesmake@gmail.com>
2018-11-06 18:46:51 +0300

    Fix constants. Fix another comments

--- src/commands/populate.js
+++ src/commands/populate.js
@@ -3,19 +3,19 @@
 const {MongoClient} = require(`mongodb`);
 const {generateEntity} = require(`../generate-entity.js`);
 
-const count = 100;
-const url = `mongodb://localhost:27017`;
-
-let client;
+const ENTITIES_COUNT = 100;
+const MONGO_SERVER_URL = `mongodb://localhost:27017`;
 
 const generate = () => {
+  let client;
+
   return new Promise((resolve, reject) => {
     const elements = [];
-    for (let i = 0; i < count; i++) {
+    for (let i = 0; i < ENTITIES_COUNT; i++) {
       elements.push(generateEntity());
     }
 
-    MongoClient.connect(url)
+    MongoClient.connect(MONGO_SERVER_URL)
       .then((clientConnection) => {
         client = clientConnection;
         const db = client.db(`offers`);
@@ -27,7 +27,7 @@ const generate = () => {
         client.close();
         return resolve();
       })
-      .catch((err) => reject(err));
+      .catch(reject);
   });
 };
 
--- src/connect.js
+++ src/connect.js
@@ -2,16 +2,18 @@
 
 const {MongoClient} = require(`mongodb`);
 
-const url = `mongodb://localhost:27017`;
+const MONGO_SERVER_URL = `mongodb://localhost:27017`;
 
 const connectAndRead = async () => {
-  const client = await MongoClient.connect(url);
+  const client = await MongoClient.connect(MONGO_SERVER_URL);
   const db = client.db(`offers`);
-  db.collection(`offers`).find({}).toArray()
-    .then((collection) => {
-      console.log(collection);
-      client.close();
-    });
+
+  const collection = await db.collection(`offers`)
+    .find({})
+    .toArray();
+
+  console.log(collection);
+  client.close();
 };
 
 connectAndRead().catch((e) => {
